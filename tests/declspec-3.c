129b53fdd6cdbe562c8a6955b882cbd84a46ce96
Alan Mackenzie <bug-cc-mode@gnu.org>
2016-04-30 15:45:33 +0000

    CC Mode: Recognize a noise macro with parens after a declarator's identifier
    * cc-engine (c-forward-decl-or-cast-1): In the while loop following comment
    "Skip over type decl suffix operators." insert code also to check for noise
    macros with parentheses.
    
    * tests/declspec-3.{c,face}: Add a test case of the above.

--- tests/declspec-3.c
+++ tests/declspec-3.c
@@ -1,7 +1,8 @@
 static __FOO (bar) int __FOO vt_event_wait_ioctl(struct vt_event __user *event);
+re_char *main_p IF_LINT (= NULL);
 
 /* Local Variables: */
 /* c-noise-macro-names: ("__user") */
-/* c-noise-macro-with-parens-names: ("__FOO") */
+/* c-noise-macro-with-parens-names: ("__FOO" "IF_LINT") */
 /* eval: (c-make-noise-macro-regexps) */
 /* End: */
