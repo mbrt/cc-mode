bcf1cc0345eb0ce8b712ae10b5f35924da7b246e
Alan Mackenzie <bug-cc-mode@gnu.org>
2016-12-31 16:58:56 +0000

    Fix edebug spec for cc-eval-when-compile to allow for instrumenting defuns
    * cc-defs.el: Correct the edebug spec for cc-eval-when-compile from t to
    (&rest def-form) so that it will insert an edebug-enter into the instrumented
    form, thus allowing functions containing it to be debugged without the
    instrumentation throwing errors.

--- cc-defs.el
+++ cc-defs.el
@@ -1213,7 +1213,7 @@ been put there by c-put-char-property.  POINT remains unchanged."
 ;; Make edebug understand the macros.
 (eval-after-load "edebug"
   '(progn
-     (def-edebug-spec cc-eval-when-compile t)
+     (def-edebug-spec cc-eval-when-compile (&rest def-form))
      (def-edebug-spec c-point t)
      (def-edebug-spec c-set-region-active t)
      (def-edebug-spec c-safe t)
